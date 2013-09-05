d3eedea11b400aa8a2426e09496e35511a0bfddd
kennib <kenni.bawden@gmail.com>
2013-09-06 07:50:39 +1000

    Fetching preload data too

--- scripts/emlparse.py
+++ scripts/emlparse.py
@@ -8,56 +8,62 @@ def candidate_id(candidate_eml):
     return int(candidate_eml['eml:CandidateIdentifier']['@Id'])
 
 
-def eml_to_JSON(eml_file):
+def eml_to_JSON(eml_file, type="media feed"):
     elect_data = xmltodict.parse(open(eml_file))
+    
+    if type == "media feed":
+      firstprefs_json = {}
+      twocandidate_json = {}
 
-    firstprefs_json = {}
-    twocandidate_json = {}
+      for election in elect_data['MediaFeed']['Results']['Election']:
+          # House of Representative contests
+          if 'House' in election:
+              for contest in election['House']['Contests']['Contest']:
+                  electorate_id = int(contest['eml:ContestIdentifier']['@Id'])
 
-    for election in elect_data['MediaFeed']['Results']['Election']:
-        # House of Representative contests
-        if 'House' in election:
-            for contest in election['House']['Contests']['Contest']:
-                electorate_id = int(contest['eml:ContestIdentifier']['@Id'])
+                  # First preference data
+                  firstprefs = contest['FirstPreferences']
+                  candidates = firstprefs['Candidate']
+                  
+                  firstprefs_json[electorate_id] = {
+                      'electorate_id': electorate_id,
+                      'total': value(firstprefs['Total']['Votes']),
+                      'formal': value(firstprefs['Formal']['Votes']),
+                      'informal': value(firstprefs['Informal']['Votes']),
+                      'votes': {
+                          candidate_id(candidate):
+                              value(candidate['Votes'])
+                          for candidate in candidates
+                      }
+                  }
 
-                # First preference data
-                firstprefs = contest['FirstPreferences']
-                candidates = firstprefs['Candidate']
-                
-                firstprefs_json[electorate_id] = {
-                    'electorate_id': electorate_id,
-                    'total': value(firstprefs['Total']['Votes']),
-                    'formal': value(firstprefs['Formal']['Votes']),
-                    'informal': value(firstprefs['Informal']['Votes']),
-                    'votes': {
-                        candidate_id(candidate):
-                            value(candidate['Votes'])
-                        for candidate in candidates
-                    }
-                }
-
-                # Two Candidate Preferred data
-                twocand = contest['TwoCandidatePreferred']
-                restricted = '@Restricted' in twocand
-                maverick = '@Maverick' in twocand
-                
-                if not(restricted) and not(maverick):
-                    candidates = twocand['Candidate']
-                else:
-                    candidates = []
-                
-                twocandidate_json[electorate_id] = {
-                    'electorate_id': electorate_id,
-                    'available': not(restricted) and not(maverick),
-                    'candidates': {
-                        candidate_id(candidate): {
-                            'candidate_id': candidate_id(candidate),
-                            'votes': value(candidate['Votes']),
-                        }
-                        for candidate in candidates
-                    }
-                }
-                
-    # Write JSON files
-    json.dump(firstprefs_json, open('firstpreferences.json', 'w'))
-    json.dump(twocandidate_json, open('twocandidate.json', 'w'))
+                  # Two Candidate Preferred data
+                  twocand = contest['TwoCandidatePreferred']
+                  restricted = '@Restricted' in twocand
+                  maverick = '@Maverick' in twocand
+                  
+                  if not(restricted) and not(maverick):
+                      candidates = twocand['Candidate']
+                  else:
+                      candidates = []
+                  
+                  twocandidate_json[electorate_id] = {
+                      'electorate_id': electorate_id,
+                      'available': not(restricted) and not(maverick),
+                      'candidates': {
+                          candidate_id(candidate): {
+                              'candidate_id': candidate_id(candidate),
+                              'votes': value(candidate['Votes']),
+                          }
+                          for candidate in candidates
+                      }
+                  }
+                  
+      # Write JSON files
+      json.dump(firstprefs_json, open('firstpreferences.json', 'w'))
+      json.dump(twocandidate_json, open('twocandidate.json', 'w'))
+      
+    elif type == "candidates":
+      pass
+    elif type == "electorates":
+      pass
--- scripts/fetchdata.py
+++ scripts/fetchdata.py
@@ -7,36 +7,70 @@ DELAY = 90 # seconds
 
 FEED_NAME = "results.aec.gov.au"
 FEED_ID = 15508
+FEED_PRELOAD= "Preload"
 FEED_DETAIL = "Standard"
 FEED_TYPE = "Verbose"
+
+# Fetches files from the AEC FTP site
+# takes a list of files to extract from the most recent zip in the given directory
+# takes a list of excluded files not to download
+# takes a boolean for vebosity of logging messages
+def fetch_files(dir, files=None, types=None, excluded=None, verbose=True):
+  if files == None: files = []
+  if types == None: types = ["media feed"]*len(files)
+  if excluded == None: excluded = []
+  
+  # Get the name of the most recent file in the gven directory
+  most_recent = ftp.nlst()[-1]
+  
+  # Download it if it is not excluded
+  if most_recent not in excluded:
+    url = "ftp://{}/{}/{}/{}".format(FEED_NAME, FEED_ID, dir, most_recent)
+    subprocess.call(["curl", "--silent", url, "-o", most_recent])
+    if verbose:
+      print("Retrieved", most_recent)
+    # Unzip the file
+    eml_files = subprocess.check_output(["unzip", "-Z", "-1", most_recent]+files).splitlines()
+    eml_files = [eml.split('/')[-1] for eml in eml_files]
+    subprocess.call(["unzip", "-qq", "-jn", most_recent]+files)
+    if verbose:
+      print("Unzipped ", most_recent)
+    # Get JSON from EML
+    for eml, type in zip(eml_files, types):
+      print("Parsing  ", eml, type)
+      emlparse.eml_to_JSON(eml, type)
+    if verbose:
+      print("To JSON  ", eml_files)
+  else:
+    if verbose:
+      print("Already retrieved the most recent file", most_recent)
+  
+  if verbose:
+    print()
+  
+  return most_recent
 
 # Setup the FTP connection
 ftp = FTP(FEED_NAME)
 ftp.login()
-ftp.cwd("{}/{}/{}".format(FEED_ID, FEED_DETAIL, FEED_TYPE))
+
+
+# Fetch preload data
+ftp.cwd("/{}/{}/{}/".format(FEED_ID, FEED_DETAIL, FEED_PRELOAD))
+dir = "{}/{}".format(FEED_DETAIL, FEED_PRELOAD)
+fetch_files(dir, files=["xml/eml-*-candidates-*.xml", "xml/eml-*-event-*.xml"],
+                 types=["candidates", "electorates"])
 
 
 # Keep polling the FTP server
+ftp.cwd("/{}/{}/{}/".format(FEED_ID, FEED_DETAIL, FEED_TYPE))
 retrieved = set()
 while True:
-    most_recent = ftp.nlst()[-1]
-
-    if most_recent not in retrieved:
-        # Retrieve most recent file
-        retrieved.add(most_recent)
-        url = "ftp://{}/{}/{}/{}/{}".format(FEED_NAME,FEED_ID,
-                                         FEED_DETAIL, FEED_TYPE, most_recent)
-        subprocess.call(["curl", "--silent", url, "-o", most_recent])
-        print("Retrieved", most_recent)
-        # Unzip the file
-        eml = subprocess.check_output(["unzip", "-Z", "-1", most_recent, "xml/*.xml"]).strip().lstrip('xml/')
-        subprocess.call(["unzip", "-qq", "-jf", most_recent, "xml/*.xml"])
-        print("Unzipped ", most_recent)
-        # Get JSON from EML
-        emlparse.eml_to_JSON(eml)
-        print("To JSON  ", most_recent)
-    else:
-        print("Already retrieved the most recent file", most_recent)
-
-    print("Waiting....")
-    time.sleep(DELAY)
+  # Fetch the latest results
+  dir = "{}/{}".format(FEED_DETAIL, FEED_TYPE)
+  new_file = fetch_files(dir, files=["xml/*.xml"], types=["media feed"], excluded=retrieved)
+  retrieved.add(new_file)
+  
+  # Wait a while before fetching the next set of results
+  print("Waiting....")
+  time.sleep(DELAY)
